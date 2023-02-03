309fac9152e53f76a835fd1aebb135523294b1fb
my_mac_dimitree54 <dimitree54@gmail.com>
2023-02-03 08:44:03 +0200

    turn off JVM desktop (as kmm firebase does not support it)

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,60 +1,61 @@
 plugins {
-	kotlin("multiplatform")
-	kotlin("native.cocoapods")
-	id("com.android.library")
+    kotlin("multiplatform")
+    kotlin("native.cocoapods")
+    id("com.android.library")
 }
 
 kotlin {
-	android()
+    android()
 
-	jvm("desktop")
+    //jvm("desktop")
 
-	ios()
-	iosSimulatorArm64()
+    ios()
+    iosSimulatorArm64()
 
-	js(IR) {
-		browser()
-	}
+    js(IR) {
+        browser()
+    }
 
-	cocoapods {
-		summary = "Shared code for the sample"
-		homepage = "https://github.com/JetBrains/compose-jb"
-		version = "1.0"
-		ios.deploymentTarget = "14.1"
-		framework {
-			baseName = "cuboc_core"
-		}
-	}
+    cocoapods {
+        summary = "Shared code for the sample"
+        homepage = "https://github.com/JetBrains/compose-jb"
+        version = "1.0"
+        ios.deploymentTarget = "14.1"
+        framework {
+            baseName = "cuboc_core"
+        }
+    }
 
-	sourceSets {
-		val commonMain by getting {
-			dependencies {
-				// implementation("org.mongodb:mongodb-driver-sync:4.8.1")
-			}
-		}
+    sourceSets {
+        val commonMain by getting {
+            dependencies {
+                // implementation("org.mongodb:mongodb-driver-sync:4.8.1")
+                implementation("dev.gitlive:firebase-firestore:1.6.2")
+            }
+        }
 
-		val androidMain by getting
+        val androidMain by getting
 
-		val iosMain by getting {
-			dependsOn(commonMain)
-		}
-		val iosSimulatorArm64Main by getting {
-			dependsOn(iosMain)
-		}
+        val iosMain by getting {
+            dependsOn(commonMain)
+        }
+        val iosSimulatorArm64Main by getting {
+            dependsOn(iosMain)
+        }
 
-		val desktopMain by getting
-	}
+        //val desktopMain by getting
+    }
 }
 
 android {
-	compileSdk = 33
-	sourceSets["main"].manifest.srcFile("src/androidMain/AndroidManifest.xml")
-	defaultConfig {
-		minSdk = 24
-		targetSdk = 33
-	}
-	compileOptions {
-		sourceCompatibility = JavaVersion.VERSION_1_8
-		targetCompatibility = JavaVersion.VERSION_1_8
-	}
+    compileSdk = 33
+    sourceSets["main"].manifest.srcFile("src/androidMain/AndroidManifest.xml")
+    defaultConfig {
+        minSdk = 24
+        targetSdk = 33
+    }
+    compileOptions {
+        sourceCompatibility = JavaVersion.VERSION_1_8
+        targetCompatibility = JavaVersion.VERSION_1_8
+    }
 }
