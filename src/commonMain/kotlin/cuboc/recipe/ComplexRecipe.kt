2e265c52218e2b3459a123f6ee6302f0c52603bf
my_mac_dimitree54 <dimitree54@gmail.com>
2023-02-05 21:49:46 +0200

    refactoring

--- src/commonMain/kotlin/cuboc/recipe/ComplexRecipe.kt
+++ src/commonMain/kotlin/cuboc/recipe/ComplexRecipe.kt
@@ -1,9 +1,7 @@
 package cuboc.recipe
 
-import cuboc.ingredient.Ingredient
 import cuboc.ingredient.RecipeInput
 import cuboc.ingredient.RecipeOutput
-import kotlin.math.min
 
 open class ComplexRecipe(
     name: String,
@@ -15,98 +13,5 @@ open class ComplexRecipe(
     override fun scale(scaleFactor: Double) {
         super.scale(scaleFactor)
         stages.forEach { it.scale(scaleFactor) }
-    }
-}
-
-class ParallelComplexRecipe private constructor(
-    name: String,
-    inputs: Set<RecipeInput>,
-    outputs: Set<RecipeOutput>,
-    instruction: Instruction,
-    stages: List<Recipe>
-) : ComplexRecipe(name, inputs, outputs, instruction, stages) {
-    companion object {
-        fun build(name: String, stages: List<Recipe>): ComplexRecipe {
-            val requiredIngredients = mutableMapOf<Ingredient, Double>()
-            val producedIngredients = mutableMapOf<Ingredient, Double>()
-            var totalDuration = 0
-            val instructionText = StringBuilder()
-            stages.forEachIndexed { index, recipe ->
-                for (input in recipe.inputs) {
-                    val amountExisting = producedIngredients.getOrElse(input.ingredient) { 0.0 }
-                    val amountToTake = min(amountExisting, input.amount)
-                    val amountToRequest = input.amount - amountToTake
-
-                    if (amountToRequest > 0) {
-                        requiredIngredients[input.ingredient] =
-                            requiredIngredients.getOrElse(input.ingredient) { 0.0 } + amountToRequest
-                    }
-                    if (amountToTake > 0) {
-                        producedIngredients[input.ingredient] =
-                            producedIngredients.getOrElse(input.ingredient) { 0.0 } - amountToTake
-                    }
-                }
-                for (output in recipe.outputs) {
-                    producedIngredients[output.ingredient] =
-                        producedIngredients.getOrElse(output.ingredient) { 0.0 } + output.amount
-                }
-                totalDuration += recipe.instruction.durationMinutes
-                instructionText.append("${index + 1}. ${recipe.instruction.text}\n")
-            }
-            return ComplexRecipe(
-                name,
-                requiredIngredients.map { RecipeInput(it.key, it.value, false) }.toSet(),
-                producedIngredients.map { RecipeOutput(it.key, it.value, false) }.toSet(),
-                Instruction(totalDuration, instructionText.toString()),
-                stages
-            )
-        }
-    }
-}
-
-
-class SequentialComplexRecipe private constructor(
-    name: String,
-    inputs: Set<RecipeInput>,
-    outputs: Set<RecipeOutput>,
-    instruction: Instruction,
-    stages: List<Recipe>
-) : ComplexRecipe(name, inputs, outputs, instruction, stages) {
-    companion object {
-        // todo ignore trivial recipes
-        fun build(name: String, stages: List<Recipe>): ComplexRecipe {
-            val requiredIngredients = mutableMapOf<Ingredient, Double>()
-            val producedIngredients = mutableMapOf<Ingredient, Double>()
-            var totalDuration = 0
-            val instructionText = StringBuilder()
-            for (recipe in stages) {
-                for (input in recipe.inputs) {
-                    val existingAmount = producedIngredients.getOrElse(input.ingredient) { 0.0 }
-                    val takenAmount = min(existingAmount, input.amount)
-                    val extraAmount = input.amount - takenAmount
-                    if (takenAmount == existingAmount) {
-                        producedIngredients.remove(input.ingredient)
-                    } else {
-                        producedIngredients[input.ingredient] = existingAmount - takenAmount
-                    }
-                    if (extraAmount > 0) {
-                        requiredIngredients[input.ingredient] =
-                            requiredIngredients.getOrElse(input.ingredient) { 0.0 } + extraAmount
-                    }
-                }
-                for (output in recipe.outputs) {
-                    producedIngredients[output.ingredient] =
-                        producedIngredients.getOrElse(output.ingredient) { 0.0 } + output.amount
-                }
-                totalDuration += recipe.instruction.durationMinutes
-            }
-            return ComplexRecipe(
-                name,
-                requiredIngredients.map { RecipeInput(it.key, it.value, false) }.toSet(),
-                producedIngredients.map { RecipeOutput(it.key, it.value, false) }.toSet(),
-                Instruction(totalDuration, instructionText.toString()),
-                stages
-            )
-        }
     }
 }
---
+++ src/commonMain/kotlin/cuboc/recipe/ParallelComplexRecipe.kt
@@ -0,0 +1,41 @@
+package cuboc.recipe
+
+import cuboc.ingredient.Ingredient
+import cuboc.ingredient.RecipeInput
+import cuboc.ingredient.RecipeOutput
+
+class ParallelComplexRecipe private constructor(
+    name: String,
+    inputs: Set<RecipeInput>,
+    outputs: Set<RecipeOutput>,
+    instruction: Instruction,
+    stages: List<Recipe>
+) : ComplexRecipe(name, inputs, outputs, instruction, stages) {
+    companion object {
+        fun build(name: String, stages: List<Recipe>): ParallelComplexRecipe {
+            val requiredIngredients = mutableMapOf<Ingredient, Double>()
+            val producedIngredients = mutableMapOf<Ingredient, Double>()
+            var totalDuration = 0
+            val instructionText = StringBuilder()
+            stages.forEachIndexed { index, recipe ->
+                for (input in recipe.inputs) {
+                    requiredIngredients[input.ingredient] =
+                        requiredIngredients.getOrElse(input.ingredient) { 0.0 } + input.amount
+                }
+                for (output in recipe.outputs) {
+                    producedIngredients[output.ingredient] =
+                        producedIngredients.getOrElse(output.ingredient) { 0.0 } + output.amount
+                }
+                totalDuration += recipe.instruction.durationMinutes
+                instructionText.append("${index + 1}. ${recipe.instruction.text}\n")
+            }
+            return ParallelComplexRecipe(
+                name,
+                requiredIngredients.map { RecipeInput(it.key, it.value, false) }.toSet(),
+                producedIngredients.map { RecipeOutput(it.key, it.value, false) }.toSet(),
+                Instruction(totalDuration, instructionText.toString()),
+                stages
+            )
+        }
+    }
+}
