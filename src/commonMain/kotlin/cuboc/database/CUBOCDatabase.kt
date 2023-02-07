5a4f25b43009e41275d621d88098ecd79399bad5
my_mac_dimitree54 <dimitree54@gmail.com>
2023-02-07 19:47:56 +0200

    update execution interface in core

--- src/commonMain/kotlin/cuboc/database/CUBOCDatabase.kt
+++ src/commonMain/kotlin/cuboc/database/CUBOCDatabase.kt
@@ -10,7 +10,7 @@ import cuboc_core.cuboc.database.search.SearchRequest
 import cuboc_core.cuboc.database.search.SearchResult
 
 interface CUBOCDatabase {
-    suspend fun execute(scenario: Scenario): PieceOfResource?
+    suspend fun execute(scenario: Scenario, requesterId: String): PieceOfResource?
     suspend fun search(request: SearchRequest): List<SearchResult>
     suspend fun removeResource(resource: UserResource): Boolean
     suspend fun removeRecipe(recipe: UserRecipe): Boolean
